1d50c1b5cfb1002346509016ac8107746fc9b3b5
tilenmiklavic <tm7321@student.uni-lj.si>
2024-04-25 16:38:49 +0200

    feat: add timestamp and url to xml output

--- corpus/webcrawler/webcrawler/spiders/spider.py
+++ corpus/webcrawler/webcrawler/spiders/spider.py
@@ -1,31 +1,35 @@
 import scrapy
+from datetime import datetime
 
 
 class SpiderSpider(scrapy.Spider):
     name = 'spider'
     allowed_domains = ['med.over.net']
-    start_urls = ['http://med.over.net/']
+    start_urls = ['https://med.over.net/']
 
     def parse(self, response):
-        print(response.status)
-        print("Here")
+
         posts = response.css('.forum-post__content ::text').getall()
-        print(len(posts))
-        print(self)
+
+        # Getting the current timestamp
+        current_time = datetime.now().isoformat()
         
         # Processing extracted content
         for post in posts:
             # Here you can further process each post if needed
             # For example, you can yield a dictionary containing the post content
             yield {
-                'content': post.strip()
+                'content': post.strip(),
+                'timestamp': current_time,  # ISO 8601 format timestamp
+                'url': response.url  # URL of the page where the post was found
             }
 
         # Extracting links and yielding new requests
         links = response.css('a::attr(href)').getall()  # This assumes all links are contained in 'a' tags and are of interest
         for link in links:
             full_url = response.urljoin(link)  # Ensures the link is a complete URL
-            if "med.over.net" in full_url:  # Optional: additional check to stay within the domain
+            print(full_url)
+            if any(domain in full_url for domain in self.allowed_domains):
                 yield scrapy.Request(full_url, callback=self.parse)
 
 
